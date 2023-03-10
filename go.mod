da677fe14ddedbbbe281c1909aff71c701c91f94
Sean Macdonald <sean.r.macdonald@gmail.com>
2023-03-10 14:49:09 -0500

    wip

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/sean9999/rebouncer
+
+go 1.20
+
+require github.com/rjeczalik/notify v0.9.3
+
+require golang.org/x/sys v0.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	rebouncer "github.com/sean9999/rebouncer/pkg"
+)
+
+var (
+	watchDir *string
+)
+
+func init() {
+	//	parse options and arguments
+	//	@todo: sanity checking
+	watchDir = flag.String("dir", ".", "what directory to watch")
+	flag.Parse()
+}
+
+func main() {
+
+	niceEvents := make(chan rebouncer.NiceEvent)
+
+	//	start watcher
+	if err := rebouncer.WatchRecursively(*watchDir, niceEvents); err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		for {
+			select {
+			case e := <-niceEvents:
+				log.Printf("%s - %s", e.Event, e.File)
+			}
+		}
+	*/
+
+	for e := range niceEvents {
+		log.Printf("%s - %s", e.Event, e.File)
+	}
+
+}
